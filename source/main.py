454b7f9f89fb2415927ce09511f12d2d414bd1e6
Martin Michaux <m.michaux@student.maastrichtuniversity.nl>
2022-12-04 15:11:47 +0100

    dataframe of words added
    TODO: link words and phrases, and then add the segments

--- source/main.py
+++ source/main.py
@@ -1,12 +1,34 @@
 from typing import List
-import source.config as cfg
+import config as cfg
+import os 
+import pandas as pd
 
-from source.transcript import read_full_transcript
+from transcript import read_full_transcript_phrase,read_full_transcript_word
 
-meeting_name = "Bdb001"
+def get_phrases_df(meeting_name):
+    path = (os.path.realpath(os.path.join(os.getcwd(), ("source\ICSI_original_transcripts\\text\\transcripts\\"+meeting_name+".mrt"))))
+    df_phrases = read_full_transcript_phrase(path)
+    # df_phrases.insert(loc=0,column='id_phrase', value=df_phrases.index)
+    return df_phrases
+
+def get_words_df(meeting_name):
+    main_path = (os.path.realpath(os.path.join(os.getcwd(), ("source\ICSI_original_transcripts\\audio\ICSIplus\Words\\"))))
+    all_files_names = [f for f in os.listdir(main_path) if os.path.isfile(os.path.join(main_path, f))]
+
+    df_words = []
+    for path in (path for path in all_files_names if meeting_name in path):
+        meeting_path = (main_path+"\\"+path)
+        participant = path[7]
+        df_words.append(read_full_transcript_word(meeting_path,participant))
+    
+    return pd.concat(df_words)
+
 
 if __name__ == "__main__":
-    words_path: List[str] = cfg.get_words_file_paths(meeting_name)
-    transcript_path: str = cfg.get_transcripts_file_path(meeting_name)
+    meeting_name = "Bdb001"
 
-    read_full_transcript(transcript_path)
+    # df_phrases = get_phrases_df(meeting_name)
+    # df_phrases.to_csv(("out\\"+meeting_name+"_phrases"), sep='\t')
+
+    df_words = get_words_df(meeting_name)
+    df_words.to_csv(("out\\"+meeting_name+"_words"), sep='\t')
--- source/transcript.py
+++ source/transcript.py
@@ -7,20 +7,53 @@ from lxml.objectify import ObjectifiedElement, StringElement
 from pandas import DataFrame
 
 
-def read_full_transcript(path: str):
-    with open(path) as file:
+def read_full_transcript_phrase(path: str):
+    with open(path,"r") as file:
         xml = objectify.parse(file)
         segment_nodes = xml.xpath("//Segment")
-        return parse_segment_nodes(segment_nodes)
+        return parse_segment_nodes_phrase(segment_nodes)
 
 
-def parse_segment_nodes(segment_nodes: List[Element]):
+def parse_segment_nodes_phrase(segment_nodes: List[Element]):
     rows = []
     for node in segment_nodes:
         row = {
             "StartTime": node.attrib["StartTime"],
             "EndTime": node.attrib["EndTime"],
             "Participant": node.attrib.get("Participant"),
+            "Text": None
+        }
+
+        raw_text = node.text
+        if raw_text is None:
+            continue
+        clean_text = raw_text.replace("\n", "").strip()
+        row["Text"] = clean_text
+        rows.append(row)
+
+        # elif isinstance(node, ObjectifiedElement):
+        #     non_vocal_node = node.find("NonVocalSound")
+        #     if non_vocal_node is not None:
+        #         row["NonVocalSound"] = non_vocal_node.attrib.get("Description")
+
+    return pd.DataFrame(rows)
+
+
+def read_full_transcript_word(path,participant):
+    with open(path,"r") as file:
+        xml = objectify.parse(file)
+        segment_nodes = xml.xpath("//w")
+        return parse_segment_nodes_word(segment_nodes,participant)
+
+def parse_segment_nodes_word(segment_nodes: List[Element],participant):
+    rows = []
+    for node in segment_nodes:
+        row = {
+            "id": node.attrib["{http://nite.sourceforge.net/}id"],
+            "StartTime": node.attrib["starttime"],
+            "EndTime": node.attrib["endtime"],
+            "Participant": participant,
+            "c": node.attrib["c"],
             "Text": None
         }
 
