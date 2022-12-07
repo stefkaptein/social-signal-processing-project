00c9ec78e8f3bf8e97dd7392f88252eb62b466f2
Martin Michaux <m.michaux@student.maastrichtuniversity.nl>
2022-12-07 16:41:56 +0100

    fix final_segmentation dataframe

--- source/main.py
+++ source/main.py
@@ -7,6 +7,9 @@ import numpy as np
 
 from transcript import read_full_transcript_phrase,read_full_transcript_word,read_full_transcript_segment
 
+meeting_name = "Bdb001"
+
+
 def get_phrases_df(meeting_name):
     path = (os.path.realpath(os.path.join(os.getcwd(), ("source\ICSI_original_transcripts\\text\\transcripts\\"+meeting_name+".mrt"))))
     df_phrases = read_full_transcript_phrase(path)
@@ -23,7 +26,7 @@ def get_words_df(meeting_name):
         df_words.append(read_full_transcript_word(meeting_path,participant))
     
     df_whole_words = pd.concat(df_words)
-    # df_whole_words = df_whole_words.reset_index(drop=True)
+    df_whole_words = df_whole_words.reset_index(drop=True)
     return df_whole_words
 
 def get_segments_df(meeting_name):
@@ -41,15 +44,17 @@ def get_segments_df(meeting_name):
     return df_whole_segments
 
 def filter_words(df_words,txt,w1):
-    word_type = df_words['id'].loc[df_words['id']==w1].values[0]
-    if "w" in word_type:
+    word_id = df_words['id'].loc[df_words['id']==w1].values[0]
+    if "w" in word_id:
         return txt
-    elif (df_words['Description'].loc[df_words['id']==w1].values[0]) and ("disfmarker" not in word_type) and ("pause" not in word_type):
+    elif (df_words['Description'].loc[df_words['id']==w1].values[0]) and ("disfmarker" not in word_id) and ("pause" not in word_id):
         txt=('[',df_words['Description'].loc[df_words['id']==w1].values[0],']')
         return"".join(map(str, txt))
-    elif "pause" in word_type:
-        duration = float(df_words['EndTime'].loc[df_words['id']==w1])-float(df_words['StartTime'].loc[df_words['id']==w1])
-        return ("[pause - "+str(round(duration,4))+"s]")
+    elif "pause" in word_id:
+        if (meeting_name+".pause.1" != word_id):
+            duration = float(df_words['EndTime'].loc[df_words['id']==w1])-float(df_words['StartTime'].loc[df_words['id']==w1])
+            return ("[Pause - "+str(round(duration,4))+"s]")
+        return "[Pause]"
     elif "disfmarker" in df_words['id'].loc[df_words['id']==w1].values[0]:
         return "[Diskmarker]"
 
@@ -59,7 +64,6 @@ def combine_df(df_words,df_segments):
     for i,row in df_final.iterrows():
         words_id = (row["words_id"]).split("#")
         words = words_id[1].split("..")
-
         w1 = words[0]
         if len(words)==1:
             txt = df_words['Text'].loc[df_words['id']==w1[3:len(w1)-1]].values[0]
@@ -76,19 +80,19 @@ def get_all_words(df_words,w1,w2,participant):
     #order df_words by start time
     index_w1 = df_words[df_words['id']==w1[3:len(w1)-1]].index.values[0]
     index_w2 = df_words[df_words['id']==w2[3:len(w2)-1]].index.values[0]
+
     words=[]
-    for i in range(index_w1-1,index_w2):
+    for i in range(index_w1,index_w2+1):
         if df_words['Participant'].iloc[i]==participant:
             txt = df_words['Text'].iloc[i]
-            words.append(filter_words(df_words,txt,df_words['id'].iloc[i]))
+            filtered_txt = filter_words(df_words,txt,df_words['id'].iloc[i])
+            words.append(filtered_txt)
     
     return " ".join(map(str, words))
 
 
 
 if __name__ == "__main__":
-    meeting_name = "Bdb001"
-
     # df_phrases = get_phrases_df(meeting_name)
     # df_phrases.to_csv(("out\\"+meeting_name+"_phrases"), sep='\t')
 
