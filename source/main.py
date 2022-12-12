96def333b2d0ee3c490d736c60b8d465de17a214
Stef Kaptein <stefkapte@gmail.com>
2022-12-12 12:05:25 +0100

    put it in a function

--- source/main.py
+++ source/main.py
@@ -2,8 +2,13 @@ import pandas as pd
 
 from source.text_features import get_sentence_similarity
 
-if __name__ == "__main__":
+
+def create_feature_vector():
     df = pd.read_csv("../out/Bed002_segments_final_sorted.csv", sep=';')
     text_features = get_sentence_similarity(df, "Text")
     audio_features = pd.read_csv("../out/Bed002_audio_features_of_segments.csv", sep=';')
-    feature_vector = audio_features.merge(text_features, left_on='segID', right_on='id', how='inner')
+    return audio_features.merge(text_features, left_on='segID', right_on='id', how='inner')
+
+
+if __name__ == "__main__":
+    feature_vector = create_feature_vector()
